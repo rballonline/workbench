c6fec225756bd6c03e70c7e22030273bfdfeb82e
William Tilton <rballonline@gmail.com>
2026-08-06 10:54:11 -0700

    Fixing deprecated test

--- spring-reactive/src/test/java/com/tiltedev/springreactive/SpringReactiveApplicationTests.java
+++ spring-reactive/src/test/java/com/tiltedev/springreactive/SpringReactiveApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
 @SpringBootTest
 @Testcontainers
@@ -15,7 +15,7 @@ class SpringReactiveApplicationTests {
 
 	@Container
 	@ServiceConnection
-	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4");
+	static MySQLContainer mysql = new MySQLContainer("mysql:8.4");
 
 	@Test
 	void contextLoads() {
